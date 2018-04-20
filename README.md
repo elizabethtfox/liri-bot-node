48264dc540742296330bf268234118effe0104df
Elizabeth Fox <elizabethtfox93@gmail.com>
2018-04-20 18:57:08 -0400

    finalized do-what-it-says command and updated README

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # liri-bot-node
+
+Developed by Elizabeth Fox
+LIRI bot is a Language Interpretation and Recognition Interface that uses CLI to search OMDB, Spotify, and Twitter's APIs.
+
+Use this CLI app to search Spotify for a song or OMDB for a movie. You can also checkout my latest tweets.
+If you don't want to make any decisions, LIRI can do it for you.
+
+Commands:
+Spotify:
+spotify-this-song <insert song name>
+
+OMDB:
+omdb-this <insert movie name>
+
+Twitter:
+my-tweets
+
+LIRI's Choice
+do-what-it-says
+
+![images-liri-1](https://user-images.githubusercontent.com/32913276/39076882-2901a33c-44cc-11e8-9109-c9a9c31e0213.png)
+
+![images-liri-2](https://user-images.githubusercontent.com/32913276/39076905-3d4b149a-44cc-11e8-956f-1d587dfd4fd4.png)
--- liri.js
+++ liri.js
@@ -6,7 +6,6 @@ var Twitter = require('twitter');
 
 // node-spotify-api npm package
 var Spotify = require('node-spotify-api');
-var spotify = new Spotify(keys.spotify);
 
 // request npm package
 var request = require('request');
@@ -29,7 +28,7 @@ theOutput();
 
 // Switch case statment for possible user inputs
 function theOutput(){
-    console.log("In the output function");
+    //console.log("In the output function");
 
     switch(userInput){
         case "my-tweets":
@@ -45,7 +44,7 @@ function theOutput(){
             getMovie();
             break;
         case 'do-what-it-says':
-            console.log("Random: ");
+            console.log("Do what it says: ");
             getRandom();
             break;
         default:
@@ -77,6 +76,8 @@ function getTweets(){
 // Spotify Function
 // Links to Spotify and Twitter keys
 function getSpotify() {
+    var spotify = new Spotify(keys.spotify);
+
     spotify.search({
         type: 'track',
         query: songName,
@@ -85,16 +86,16 @@ function getSpotify() {
         function(err,data){
         if(err){
             return console.log('Error occured: ' + err);
-        } else{
-            console.log(data);
-            var songArtist = data.tracks.artists;
-            var songAlbum = data.tracks.album;
-            var songTitle = data.track.name;
+        }else{
+            //console.log(data);
+            var songArtist = data.tracks.items[0].artists[0].name;
+            var songAlbum = data.tracks.items[0].album.name;
+            var songTitle = data.tracks.items[0].name;
 
             console.log("===========================")
             console.log("Artists: " + JSON.stringify(songArtist));
             console.log("Album: " + songAlbum);
-            console.log("Title: " + songTitle);
+            console.log("Song Title: " + songTitle);
         }
     });
 }
@@ -115,9 +116,9 @@ function getMovie(){
         console.log("IMDB Rating: " + body.imdbRating);
         console.log("Country: " + body.Country);
         console.log("Language: " + body.Language);
-        console.log("Plot: " + body.Plot);
         console.log("Actors: " + body.Actors);
         console.log("Rotten Tomatoes Rating: " + body.Ratings[1].Value);
+        console.log("\nPlot: " + body.Plot);
     });
 
 };
@@ -125,14 +126,16 @@ function getMovie(){
 // Do what it says Function
 function getRandom(){
     fs.readFile('random.txt','utf8',function(err,data){
-        if(err){
-            console.log("error");
-        } else {
-            input = data.split(",");
-            userInput = input[0];
-            songName = input[1];
-            theOutput();
-            console.log(data);
+        data = data.split(",");
+        userInput = data[0];
+        songName = data[1];
+        // console.log(data);
+        // console.log(userInput);
+        // console.log(songName);
+        if(userInput === "spotify-this-song" && songName === "undefinded"){
+            getSpotify("Wonderwall")
+        } else if (userInput === "spotify-this-song"){
+            getSpotify(songName);
         }
-    })
-}
+    });
+};
